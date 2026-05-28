1e65e596f4b3c85562dabd7c475e3b016a29af8a
allexandrelucas0-blip <allexandrelucas0@gmail.com>
2026-05-28 19:16:35 +0000

    Preencheu as lacunas

--- backend/db.js
+++ backend/db.js
@@ -1,64 +1,12 @@
-import mysql from "mysql2/promise";
+import Database from "better-sqlite3";
 
-/*
-  Este arquivo é responsável pela conexão com o banco de dados MySQL.
+export const db = new Database("banco.db");
 
-  Atenção:
-  - O front-end nunca deve se conectar diretamente ao banco.
-  - A conexão com o banco deve ser feita pelo back-end.
-  - Os dados abaixo precisam estar de acordo com a configuração do MySQL local.
-*/
-
-export const db = await mysql.createPool({
-  /*
-    LACUNA 1:
-    Informe o endereço do servidor do banco de dados.
-
-    Orientações:
-    - Em ambiente local, geralmente usamos o servidor local da máquina.
-    - O valor deve ser uma string.
-  */
-  host: ______________________,
-
-  /*
-    LACUNA 2:
-    Informe o usuário do MySQL.
-
-    Orientações:
-    - Em muitos ambientes locais, o usuário padrão costuma ser o administrador do MySQL.
-    - O valor deve ser uma string.
-  */
-  user: ______________________,
-
-  /*
-    LACUNA 3:
-    Informe a senha do usuário do MySQL.
-
-    Orientações:
-    - Essa senha é a mesma configurada na instalação do MySQL.
-    - O valor deve ser uma string.
-    - Caso não exista senha no ambiente local, analise com o professor como preencher.
-  */
-  password: ________________________,
-
-  /*
-    LACUNA 4:
-    Informe o nome do banco de dados que será utilizado no projeto.
-
-    Orientações:
-    - O nome deve ser o mesmo banco criado no script SQL.
-    - O valor deve ser uma string.
-  */
-  database: _________________________________,
-  /**
-   * Verificar a porta de conexão com o banco de dados.
-   * 
-   * Orientações:
-   * - Verificar a conexão que esstá no MySQL Workbench e porta que está nessa
-   * conexão.
-   */
-  port: __________________,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+db.exec(`
+    create table If Not EXISTS users (
+    id_users INTEGER PRIMARY KEY AUTOINCREMENT,
+    name  TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL
+    )
+    `);
